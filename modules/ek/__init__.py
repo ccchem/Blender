d6ce6e4d82930c60842bae220c72f9b14b1d8264
Me <test@test.com>
2017-08-19 12:14:51 -0700

    Vec3

---
+++ modules/ek/__init__.py
@@ -0,0 +1,1 @@
+from ek.vec3 import Vec3
---
+++ modules/ek/vec3.py
@@ -0,0 +1,11 @@
+class Vec3:
+
+    def __init__(self, x=0.0, y=0.0, z=0.0):
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+
+
+    def __str__(self):
+        return "(" + str(self.x) + ", " + str(self.y) + ", " + str(self.z) + ")"
+
